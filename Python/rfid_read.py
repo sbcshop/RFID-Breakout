653d43231b46961d103d6e6947cc9197f84fa71c
SB Components <gajender.singh@sb-components.co.uk>
2020-11-23 14:33:00 +0530

    Update rfid_read.py

--- Python/rfid_read.py
+++ Python/rfid_read.py
@@ -1,7 +1,9 @@
 import serial                          #import serial module
 
 def read_rfid ():
-   ser = serial.Serial ("/dev/ttyS0")  #Open named port, for win use "COM1"(if port is 1) 
+   ser = serial.Serial ("/dev/ttyS0")  #Open named port, if using RXD , TXD pin
+   #ser = serial.Serial ("/dev/ttyUSB0") # with raspberry pi/linux usb interface
+   #ser = serial.Serial ("COM2") # with windows, use assigned com port number from device manager
    ser.baudrate = 9600                 #Set baud rate to 9600
    data = ser.read(12)                 #Read 12 characters from serial port to data
    ser.close ()                        #Close port
